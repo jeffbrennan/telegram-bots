4d1d14ff3abdc61b5d68c1b9facc38bb0eea8064
jeffbrennan <jeffbrennan10@gmail.com>
2019-09-09 15:49:12 -0500

    add  verify + remove get request reposting

--- recipe-organizer/organizer.py
+++ recipe-organizer/organizer.py
@@ -1,45 +1,26 @@
 import requests
 import pandas as pd
-import json
+# import json
 
-# gets API key from separate file for privacy
-key_get = open('keys/telegram_key.txt', 'r')
+# Get api key
+key_get = open('recipe-organizer/keys/telegram_key.txt', 'r')
 API_KEY = key_get.read()
 
-# Read telegram IDs
-user_ids = pd.read_csv('keys/id.csv', sep=',')
+# Get telegram unique ids
+user_ids = pd.read_csv('recipe-organizer/keys/id.csv', sep=',')
 
-all_recipes = {
-               'jeff': {},
-               'lauren': {}
-              }
-
+all_recipes = {'jeff': {},
+               'lauren': {}}
 
 # ============== TESTING ============
-# with open("test.json") as f:
+# with open("recipe-organizer/test.json") as f:
 #     data = json.load(f)
-# print('http' in data['result'][2]['message']['text'].split())
-# print(data['result'][2]['message']['text'].split()[-1])
-# recipes = all_recipes['jeff']
-# print(recipes)
-# for recipe in recipes:
-#     if 'tilapia' in recipes[recipe]['tags']:
-#         print(recipes[recipe]['link'])
 
-
-# for recipe in all_recipes:
-#     if tag_search in recipe['tags']:
-#         matching_links.append(recipe['link'])
-
-
+# results = data['result']
 # ===================================
 
 
-# TODO: simplify logic
 def tag_searcher(tag_search, user):
-    print(tag_search)
-    print(user)
-    print(all_recipes)
     matching_links = []
     recipes = all_recipes[user]
 
@@ -49,63 +30,46 @@ def tag_searcher(tag_search, user):
 
     return matching_links
 
-# TODO: see if it is possible to self delete bot responses
-# def clear_output():
-# # for message in all messages
-#     # if message is from bot
-#         # add id to list 
-
-# # for id in list
-#     # post delete request
-
-#     for message in all_messages:
-#         if message['']
-
 
 # TODO: add handling so that only the last get request is processed
-def send_recipe(message):
-    message_parse = message['message']['text'].split()
+def send_recipe(result, message_parse):
+    # message_parse = message['message']['text'].split()
     tag_search = message_parse[1]
     user = message_parse[2]
     links = tag_searcher(tag_search, user)
 
     user_id = user_ids.loc[user_ids['user'] == user, 'id'][0]
 
-    # delete previous messages to prevent spamming
-    # clear_output()
-
     for link in links:
         SEND_MESSAGE = ('https://api.telegram.org/bot' + API_KEY
                         + '/sendmessage?chat_id=' + str(user_id)
                         + '&text=' + link)
+        requests.get(SEND_MESSAGE)
 
-    requests.get(SEND_MESSAGE)
-
-
-def shortcut_validation(shortcut):
-    try:
-        int(shortcut)
-        return True
-    except ValueError:
+    
+def validate_recipe(user, shortcut):
+    if user == 'jeff' or user == 'lauren':
+        try:
+            int(shortcut)
+            return True
+        except ValueError:
+            return False
+    else:
         return False
 
 
-def add_recipe(message, index, all_messages):
-    link = message['message']['text'].split()[-1]
-    user = all_messages[index+1]['message']['text']
-    shortcut = all_messages[index+2]['message']['text']
-    tags = all_messages[index+3]['message']['text']
+def add_recipe(result, message_parse, index, all_results):
+    link = message_parse[-1]
+    user = all_results[index + 1]['message']['text']
+    shortcut = all_results[index + 2]['message']['text']
+    tags = all_results[index + 3]['message']['text']
 
     print(link)
     print(user)
     print(shortcut)
     print(tags)
 
-    # TODO: simplify logic
-    # TODO add check for tags
-    if user != 'jeff' and user != 'lauren':
-        if not shortcut_validation(shortcut):
-            pass
+    if not validate_recipe(user, shortcut):
         pass
     else:
         sub_dict = {shortcut: {'link': link, 'tags': tags}}
@@ -135,35 +99,56 @@ def add_recipe(message, index, all_messages):
 #     recipes.to_csv('data/recipes.csv')
 
 
-def message_handler(all_messages):
-    for index, message in enumerate(all_messages):
-        # validate_input(message)
-        message_parse = message['message']['text'].lower().split()
-        command = message_parse[0]
-        if command == 'get':
-            send_recipe(message)
+def get_logger(all_results):
+    get_ids = []
+
+    for result in all_results:
+        if 'get' in result['message']['text'].split():
+            get_ids.append(result['update_id'])
+    return get_ids
+
+
+def result_handler(all_results):
+    get_ids = get_logger(all_results)
+    print(get_ids)
+    # for result in results:
+    #     if 'get' in result['message']['text'].split():
+    #         get_ids.append(result['update_id'])
+
+    for index, result in enumerate(all_results):
+        # validate_input(result)
+        update_id = result['update_id']
+
+        message_parse = result['message']['text'].lower().split()
+        print(message_parse)
+        # command = message_parse[0]
+
+        if update_id == get_ids[-1]:
+            send_recipe(result, message_parse)
+            print('Successful push. Result index: ' + str(index))
         elif 'http' in message_parse[-1]:
-            add_recipe(message, index, all_messages)
+            add_recipe(result, message_parse, index, all_results)
+            print('Successful add. Result index: ' + str(index))
         # elif command == 'edit':
         #     edit_recipe(message)
         # elif command == 'delete':
             # delete_message(message, index, all_messages)
         else:
-            print('N/A text. Message index: ' + str(index))
+            print('N/A text. Result index: ' + str(index))
 
 
-def receive_message():
+def receive_updates():
     GET_UPDATES = ('https://api.telegram.org/bot' + API_KEY + '/getUpdates')
-    message_response = requests.get(GET_UPDATES)
-    all_messages = message_response.json()
+    response = requests.get(GET_UPDATES)
+    all_results = response.json()
 
-    return all_messages['result']
+    return all_results['result']
 
 
 def main():
     print(all_recipes)
-    all_messages = receive_message()
-    message_handler(all_messages)
+    all_results = receive_updates()
+    result_handler(all_results)
     print(all_recipes)
 
 
